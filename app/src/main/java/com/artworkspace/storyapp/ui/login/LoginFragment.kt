956ce885c47e7d978e08c66838b7f7bd2a73211f
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-04-07 23:51:27 +0700

    create PasswordEditText.kt and EmailEditText.kt

--- app/src/main/java/com/artworkspace/storyapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/artworkspace/storyapp/ui/login/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.artworkspace.storyapp.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.artworkspace.storyapp.MainActivity
 import com.artworkspace.storyapp.R
 import com.artworkspace.storyapp.databinding.FragmentLoginBinding
 
@@ -28,6 +31,16 @@ class LoginFragment : Fragment() {
         binding.btnRegister.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registerFragment)
         )
+
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(requireContext(), "Welcome", Toast.LENGTH_SHORT)
+                .show()
+
+            Intent(requireContext(), MainActivity::class.java).also {
+                startActivity(it)
+                requireActivity().finish()
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/artworkspace/storyapp/views/EmailEditText.kt
@@ -0,0 +1,69 @@
+package com.artworkspace.storyapp.views
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import com.artworkspace.storyapp.R
+
+class EmailEditText : AppCompatEditText {
+
+    private lateinit var emailIconDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        emailIconDrawable =
+            ContextCompat.getDrawable(context, R.drawable.ic_baseline_email_24) as Drawable
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        compoundDrawablePadding = 16
+
+        setHint(R.string.email)
+        setAutofillHints(AUTOFILL_HINT_EMAIL_ADDRESS)
+        setDrawable(emailIconDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty() && !Patterns.EMAIL_ADDRESS.matcher(s).matches())
+                    error = context.getString(R.string.et_email_error_message)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // Do nothing
+            }
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
+}
